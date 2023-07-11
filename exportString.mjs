cf98a4e18b7ae8837fd33e36ff7ab867f585909e
vivekrudra980 <vivekprudra@gmail.com>
2023-07-11 05:13:50 +0000

    exporting a string

---
+++ exportString.mjs
@@ -0,0 +1,10 @@
+// Create two files exportString.mjs, importString.mjs parallel to README.md file.
+
+// Write a string Be Happy and Safe in the file exportString.mjs and export it using the default export syntax.
+
+// Import the string in the importString.mjs file.
+
+// Use ES6 module syntax.
+
+let string = "Be Happy and Safe";
+export default string;
